0d07f5aa855af036bde3aa075642b9880b24fe32
Bagus Miftaq Hurohman <bagusmiftaq@gmail.com>
2023-01-02 12:01:40 +0700

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -6,40 +6,58 @@ import AddType from "./pages/AddType";
 import TypeList from "./pages/TypeList";
 import CourseList from "./pages/CourseList";
 import NavBar from "./components/NavBar";
-import {Routes, Route, Outlet} from "react-router-dom";
+import {Routes, Route, Outlet, Navigate} from "react-router-dom";
 import EditCourse from "./pages/EditCourse";
 
-const CourseWrapper=()=>(
+const CourseWrapper = () => (
     <>
         <h1>Course Page</h1>
         <Outlet/>
     </>
 )
 
-const TypeWrapper=()=>(
+const TypeWrapper = () => (
     <>
         <h1>Type Course Page</h1>
         <Outlet/>
     </>
 )
 
+const ProtectedRoutes = ({isLoggedIn}) => {
+    if (!isLoggedIn) {
+        return <Navigate to={ROUTES.LOGIN} replace={true}/>
+    }
+
+    return (
+        <>
+            <NavBar/>
+            <hr/>
+            <Outlet/>
+        </>
+    )
+}
+
 function App() {
+
+    const [isLoggedin, setIsLoggedIn] = React.useState(true);
 
     return (
         <div className="App">
-            <NavBar/>
+
             <Routes>
-                <Route path={ROUTES.DASHBOARD} element={<h1>Dashboard Page</h1>} index={true}/>
-                <Route path={ROUTES.COURSE_LIST} element={<CourseWrapper/>}>
-                    <Route element={<CourseList/>} index={true}/>
-                    <Route path={ROUTES.ADD_COURSE} element={<AddCourse/>}/>
-                    <Route path={`${ROUTES.EDIT_COURSE}/:courseId?`} element={<EditCourse/>}/>
+                <Route path={ROUTES.LOGIN} element={<h1>Login Page</h1>} index={true}/>
+                <Route path={ROUTES.DASHBOARD} element={<ProtectedRoutes isLoggedIn={isLoggedin}/>}>
+                    <Route path={ROUTES.COURSE_LIST} element={<CourseWrapper/>}>
+                        <Route element={<CourseList/>} index={true}/>
+                        <Route path={ROUTES.ADD_COURSE} element={<AddCourse/>}/>
+                        <Route path={`${ROUTES.EDIT_COURSE}/:courseId?`} element={<EditCourse/>}/>
+                    </Route>
+                    <Route path={ROUTES.TYPE_LIST} element={<TypeWrapper/>}>
+                        <Route element={<TypeList/>} index={true}/>
+                        <Route path={ROUTES.ADD_TYPE} element={<AddType/>}/>
+                    </Route>
+                    <Route path={"*"} element={<h3>Page not Found</h3>}/>
                 </Route>
-                <Route path={ROUTES.TYPE_LIST} element={<TypeWrapper/>}>
-                    <Route element={<TypeList/>} index={true}/>
-                    <Route path={ROUTES.ADD_TYPE} element={<AddType/>}/>
-                </Route>
-                <Route path={"*"} element={<h3>Page not Found</h3>}/>
             </Routes>
 
         </div>
